73a39d7d48875c161f6c55d76cc9fc740d3a7680
Tips <150616801+Tips-Discord@users.noreply.github.com>
2024-06-11 13:21:56 +0200

    Update Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -1,37 +1,33 @@
-import requests
-import colorama 
 import os
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
 
 os.system(f"title Cwelium Downloader")
 
-stars = requests.get(f"https://api.github.com/repos/Tips-Discord/Cwelium").json()["stargazers_count"]
+def download_latest_release():
+    try:
+        response = requests.get("https://api.github.com/repos/Tips-Discord/Cwelium/releases/latest")
+        response.raise_for_status()
 
-menu = f"""
+        response = requests.get(response.json()['assets'][0]['browser_download_url'])
+        response.raise_for_status()
+
+        with open(f"Cwelium.exe", "wb") as file:
+            file.write(response.content) 
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    print(f"""
 {'██████╗  ██████╗ ██╗    ██╗███╗   ██╗██╗      ██████╗  █████╗ ██████╗ ███████╗██████╗ '.center(os.get_terminal_size().columns)}
 {'██╔══██╗██╔═══██╗██║    ██║████╗  ██║██║     ██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗'.center(os.get_terminal_size().columns)}
 {'██║  ██║██║   ██║██║ █╗ ██║██╔██╗ ██║██║     ██║   ██║███████║██║  ██║█████╗  ██████╔╝'.center(os.get_terminal_size().columns)}
 {'██║  ██║██║   ██║██║███╗██║██║╚██╗██║██║     ██║   ██║██╔══██║██║  ██║██╔══╝  ██╔══██╗'.center(os.get_terminal_size().columns)}
 {'██████╔╝╚██████╔╝╚███╔███╔╝██║ ╚████║███████╗╚██████╔╝██║  ██║██████╔╝███████╗██║  ██║'.center(os.get_terminal_size().columns)}
 {'╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝'.center(os.get_terminal_size().columns)}
-{f'Stars {stars}'.center(os.get_terminal_size().columns)}"""
-
-def download_latest_release():
-    try:
-        response = requests.get("https://api.github.com/repos/Tips-Discord/Cwelium/releases/latest")
-        response.raise_for_status()
-        release_info = response.json()
-        asset_url = release_info['assets'][0]['browser_download_url']
-
-        response = requests.get(asset_url)
-        response.raise_for_status()
-
-        with open(f"Cwelium.exe", "wb") as file:
-            file.write(response.content)
-        
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    print(menu)
+{f'Stars {requests.get(f"https://api.github.com/repos/Tips-Discord/Cwelium").json()["stargazers_count"]}'.center(os.get_terminal_size().columns)}""")
     download_latest_release()
     os.system("start Cwelium.exe")
